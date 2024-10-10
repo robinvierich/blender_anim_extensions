6bd7bf6d39775115bedf2e05e67cacc8c24d8223
robin <robinvierich@gmail.com>
2024-10-10 16:27:20 -0400

    moved get_selected_keyframes to utils/

--- insert_remove_inbetween.py
+++ insert_remove_inbetween.py
@@ -4,6 +4,7 @@ import bpy
 from bpy.types import Panel, Operator, PropertyGroup 
 from bpy.props import IntProperty
 
+from utils.keyframe_utils import get_selected_keyframes
 
 class InsertRemoveProps(PropertyGroup):
     frames: IntProperty(
@@ -13,30 +14,6 @@ class InsertRemoveProps(PropertyGroup):
         min=1,
         max=10
     ) # type: ignore
-
-
-def get_selected_keyframes(context : bpy.types.Context):
-    if context.area.type == 'DOPESHEET_EDITOR':
-        obj = context.active_object
-        action = obj.animation_data.action if obj and obj.animation_data else None
-
-        if not action:
-            return []
-
-        all_keyframes = []
-
-        for fcurve in action.fcurves: 
-            all_keyframes.extend(fcurve.keyframe_points.values())
-
-        all_selected_keyframes = [keyframe for keyframe in all_keyframes if keyframe.select_control_point]
-
-        return all_selected_keyframes
-
-    elif context.area.type == 'GRAPH_EDITOR':
-        return context.selected_editable_keyframes
-    
-    return []
-
 
 
 # Operators for button functionality
---
+++ utils/keyframe_utils.py
@@ -0,0 +1,24 @@
+
+from bpy.types import Context
+
+def get_selected_keyframes(context : Context):
+    if context.area.type == 'DOPESHEET_EDITOR':
+        obj = context.active_object
+        action = obj.animation_data.action if obj and obj.animation_data else None
+
+        if not action:
+            return []
+
+        all_keyframes = []
+
+        for fcurve in action.fcurves: 
+            all_keyframes.extend(fcurve.keyframe_points.values())
+
+        all_selected_keyframes = [keyframe for keyframe in all_keyframes if keyframe.select_control_point]
+
+        return all_selected_keyframes
+
+    elif context.area.type == 'GRAPH_EDITOR':
+        return context.selected_editable_keyframes
+    
+    return []
